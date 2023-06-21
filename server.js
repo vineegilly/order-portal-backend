f57474f6681e2e9dd413189d481b67e11ee4d8a0
vineethkumar.kota <vineegilly@gmail.com>
2023-06-22 01:00:28 +0530

    basic express server

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// Enable All CORS Requests
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  const dummyData =   [
+    {
+      "id": "001",
+      "category": "Furniture",
+      "subCategory": "Tables",
+      "segment": "Corporate",
+      "productName": "GE General",
+      "orderDate": "2023-06-21",
+      "region": "West",
+      "profit": 50,
+      "sales": 300,
+      "imageUrl": "https://picsum.photos/500/300",
+      "lineItems":[
+        {
+          "id": "001-01",
+          "item": "TableA",
+          "profit": 3,
+          "sales": 100
+        },
+        {
+          "id": "001-02",
+          "item": "TableB",
+          "profit": 1,
+          "sales": 150
+        }
+      ]
+    },
+    {
+      "id": "002",
+      "category": "Furniture",
+      "subCategory": "Chairs",
+      "segment": "Home Office",
+      "productName": "Bevis Round",
+      "orderDate": "2023-06-20",
+      "region": "East",
+      "profit": 70,
+      "sales": 400,
+      "imageUrl": "https://picsum.photos/500/300",
+      "lineItems":[
+        {
+          "id": "002-01",
+          "item": "ChairA",
+          "profit": 3,
+          "sales": 100
+        },
+        {
+          "id": "002-02",
+          "item": "ChairB",
+          "profit": 1,
+          "sales": 150
+        }
+      ]
+    }
+  ]
+
+  res.json(dummyData);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
